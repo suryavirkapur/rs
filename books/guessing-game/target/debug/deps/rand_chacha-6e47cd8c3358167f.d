46fad6f081fec10d4c90d9339d26930505ae9483
root <root@Cync.localdomain>
2021-06-24 19:26:19 +0530

    guessing game finished

--- books/guessing-game/target/debug/deps/rand_chacha-6e47cd8c3358167f.d
+++
@@ -1,9 +0,0 @@
-/home/rs/books/guessing-game/target/debug/deps/rand_chacha-6e47cd8c3358167f.rmeta: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/rs/books/guessing-game/target/debug/deps/librand_chacha-6e47cd8c3358167f.rlib: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/rs/books/guessing-game/target/debug/deps/rand_chacha-6e47cd8c3358167f.d: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
-/root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
-/root/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
--- books/guessing-game/target/release/build/libc-48d8cc64c59c0110/build_script_build-48d8cc64c59c0110.d
+++
@@ -1,5 +0,0 @@
-/home/rs/books/guessing-game/target/release/build/libc-48d8cc64c59c0110/build_script_build-48d8cc64c59c0110: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.97/build.rs
-
-/home/rs/books/guessing-game/target/release/build/libc-48d8cc64c59c0110/build_script_build-48d8cc64c59c0110.d: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.97/build.rs
-
-/root/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.97/build.rs:
